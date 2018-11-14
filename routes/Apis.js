178614bbbf29ac6836a2ab1ab55cf1648ffc4549
Luis Ortiz <ortizlu@icloud.com>
2018-11-14 10:53:25 -0500

    removed unecessary dependencies

--- routes/Apis.js
+++ routes/Apis.js
@@ -1,15 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const jwt = require('jwt-simple')
-const passport = require('../config/passport')
-const config = require('../config/config')
-const User = require('../models/userModel.js')
-const bcrypt = require('bcrypt-nodejs')
-const jwtDecode = require('jwt-decode')
 const axios = require('axios')
 
-router.get('/yelp', (req,res) => {
-  axios.get(`https://api.yelp.com/v3/businesses/search?term=chicken&latitude=38.855067&longitude=-77.10720859999999`, {
+router.post('/yelp', (req,res) => {
+  axios.get(`https://api.yelp.com/v3/businesses/search?term=${req.body.foodChoice}&latitude=${req.body.latitude}&longitude=${req.body.longitude}`, {
     headers: {
       "Authorization": "Bearer " + process.env.YELP_KEY
     }
