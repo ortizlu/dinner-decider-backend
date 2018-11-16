5dd7666a13f91d1a20b73009b9a5afda911417a7
Luis Ortiz <ortizlu@icloud.com>
2018-11-16 14:10:02 -0500

    added cors in yelp api

--- routes/Apis.js
+++ routes/Apis.js
@@ -5,6 +5,7 @@ const axios = require('axios')
 router.post('/yelp', (req,res) => {
   axios.get(`https://api.yelp.com/v3/businesses/search?term=${req.body.foodChoice}${req.body.extension}`, {
     headers: {
+      "Access-Control-Allow-Origin": "*",
       "Authorization": "Bearer " + process.env.YELP_KEY
     }
   }).then(response => {
