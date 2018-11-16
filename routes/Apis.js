a197e26d8860d549563964ecc98bb04d8dfdb346
Luis Ortiz <ortizlu@icloud.com>
2018-11-16 14:37:55 -0500

    ...

--- routes/Apis.js
+++ routes/Apis.js
@@ -6,7 +6,7 @@ router.post('/yelp', (req,res) => {
   axios.get(`https://api.yelp.com/v3/businesses/search?term=${req.body.foodChoice}${req.body.extension}`, {
     headers: {
       "Access-Control-Allow-Origin": "*",
-      "Authorization": "Bearer " + process.env.YELP_KEY
+      "Authorization": "Bearer " + UF9X78Hx3MO2nRpV1PwxRqI4SycN4GeruXlSfJAaPU_cFq-TEnQ1JbvV752RTn7eOP1HUYs6tm7Gm1thgJ1fGJz7TXkAgNdEQcM_xHsoYmTFGuMMt2sQhaDt1ODmW3Yx
     }
   }).then(response => {
     res.json(response.data)
