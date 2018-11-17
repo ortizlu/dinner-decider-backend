8eb911eb41f5c733e1ddc9fbbd5c875882323dab
Luis Ortiz <ortizlu@icloud.com>
2018-11-17 13:04:38 -0500

    removed next

--- routes/Apis.js
+++ routes/Apis.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const axios = require('axios')
 
-router.post('/yelp', (req,res, next) => {
+router.post('/yelp', (req,res) => {
   axios.get(`https://api.yelp.com/v3/businesses/search?term=${req.body.foodChoice}${req.body.extension}`, {
     headers: {
       "Authorization": "Bearer " + process.env.YELP_KEY
