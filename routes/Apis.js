1820b294a5980c51c567ff470f4dbcc80bd01b7f
Luis Ortiz <ortizlu@icloud.com>
2018-11-17 13:05:21 -0500

    removed bearer

--- routes/Apis.js
+++ routes/Apis.js
@@ -5,7 +5,7 @@ const axios = require('axios')
 router.post('/yelp', (req,res) => {
   axios.get(`https://api.yelp.com/v3/businesses/search?term=${req.body.foodChoice}${req.body.extension}`, {
     headers: {
-      "Authorization": "Bearer " + process.env.YELP_KEY
+      "Authorization": process.env.YELP_KEY
     }
   }).then(response => {
     res.json(response.data)
